d0c08fef901a945f8d309e5d874ce3ec952ad11d
tejc999999 <tejc999999@gmail.com>
2018-11-13 17:47:43 +0900

    first commit.

---
+++ src/main/java/jp/spring/boot/TypingscoreApplication.java
@@ -0,0 +1,12 @@
+package jp.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypingscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypingscoreApplication.class, args);
+	}
+}
---
+++ src/main/java/jp/spring/boot/controller/TypingscoreController.java
@@ -0,0 +1,14 @@
+package jp.spring.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TypingscoreController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "this is Spring Boot sample.";
+    }
+
+}
